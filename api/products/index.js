b8ae1179e5c75d31a1b19a61fa5119d1dca7a5ed
BruceLane <bruce@batchass.fr>
2020-05-23 23:34:24 +0200

    del azure.yml

--- api/products/index.js
+++ api/products/index.js
@@ -1,7 +1,28 @@
 const data = require('../shared/product-data');
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    context.res.status(200).json(data);
+    context.res.status(200).json({
+        products: [
+            {
+              id: 10,
+              name: 'Strawberries',
+              description: '16oz package of fresh organic strawberries',
+              quantity: '1',
+            },
+            {
+              id: 20,
+              name: 'Sliced bread',
+              description: 'Loaf of fresh sliced wheat bread',
+              quantity: 1,
+            },
+            {
+              id: 30,
+              name: 'Apples',
+              description: 'Bag of 7 fresh McIntosh apples',
+              quantity: 1,
+            },
+          ]
+      });
     /*if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200,  Defaults to 200 
